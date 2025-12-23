1e3f7254c2369c5ba2664f4e94941872f536d5bd
Chiranjeevi R <iamchiranjeeviramakrishna@gmail.com>
2025-12-23 20:07:12 +0530

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,12 @@ import { Outlet } from "react-router";
 import Sidebar from "./components/Sidebar";
 import { useEffect } from "react";
 import { initStorage } from "./utils/initStorage";
-import { addTransaction } from "./utils/helper";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   useEffect(() => {
     initStorage();
-
-    addTransaction({
-      id: crypto.randomUUID(),
-      accountId: "acc-1",
-      date: "2025-02-01",
-      description: "ATM Withdrawal",
-      amount: 2000,
-      type: "debit",
-      category: "withdrawal",
-    });
   }, []);
 
   return (
